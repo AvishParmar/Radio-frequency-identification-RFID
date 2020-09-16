a8b6fa669a1b3467f9d736610d52f6f1aaa27c18
Avish Parmar <46851126+AvishParmar@users.noreply.github.com>
2020-09-16 09:25:07 -0400

    Update InvalidRFIDException.java

--- InvalidRFIDException.java
+++ InvalidRFIDException.java
@@ -1,11 +1,6 @@
-package Homework2;
 /**
  * InvalidRFIDException class that is invoked when an InvalidRFIDException is thrown.
  * @author Avish Parmar
- * SBUID: 112647892
- * Email: avish.parmar@stonybrook.edu
- * Course: CSE214
- * Recitation: Section 01
  */
 public class InvalidRFIDException extends Exception {
 	/**
